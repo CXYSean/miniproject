8f8c3af4cf5495af0ce0c172caa6e5348bed1712
Xiyu Chen <cxy.sh@icloud.com>
2020-05-11 21:20:17 -0400

    First Upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+	return render_template('main.html')
+
+@app.route('/trend',methods=['GET'])
+def trend():
+	return render_template('trend.html')
+
+@app.route('/models',methods=['GET'])
+def models():
+	return render_template('model.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
